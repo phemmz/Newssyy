901b87ce57039282665758296ebab816d32b8ad4
phemmz <phemmzmcllroy@gmail.com>
2018-02-28 17:10:20 +0100

    Use Facades url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,12 +12,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         //
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 
