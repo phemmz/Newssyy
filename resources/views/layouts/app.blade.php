4928660521e7cf5563e361d37615683942e2abd5
phemmz <phemmzmcllroy@gmail.com>
2018-02-28 16:42:04 +0100

    fix css not loading on staging

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
       <title>@yield('title')</title>
       <link href="https://fonts.googleapis.com/css?family=Nanum+Myeongjo" rel="stylesheet">
-      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+      <link rel="stylesheet" href="{{ secure_asset('css/app.css', true) }}">
   </head>
   <body>
     <div id="appendDivNews">
@@ -29,5 +29,5 @@
   </body>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ secure_asset('js/app.js') }}"></script>
 </html>
